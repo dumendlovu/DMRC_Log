5b86622df94aec66b3fd62c2875965cc43af6560
Unknown <dumendlovu@gmail.com>
2018-06-05 11:22:20 +0200

    Part one: HTML Form
    The form for the project and _POST and php code

---
+++ _program_source/confirm.php
@@ -0,0 +1,70 @@
+<?php
+//Patient Info
+$pateLastName = $_POST['pateLastName'];
+$pateFirstName = $_POST['pateFirstName'];
+$pateIntial = $_POST['pateIntial'];
+$gender = $_POST['gender'];
+$dob = $_POST['dob'];
+//Medical Aid information
+$mAProvider = $_POST['mAProvider'];
+$MAOther = $_POST['MAOther'];
+$memNumber = $_POST['memNumber'];
+$suffixNumber = $_POST['suffixNumber'];
+$memLastName = $_POST['memLastName'];
+$memFirstName = $_POST['memFirstName'];
+$r2Member = $_POST['r2Member'];
+//Visit Informaton
+$refDoctor = $_POST['refDoctor'];
+$reqExam = $_POST['reqExam'];
+//Contact Informaton
+$mobNum = $_POST['mobNum'];
+$email = $_POST['email'];
+$inputAddress = $_POST['inputAddress'];
+$inputAddress2 = $_POST['inputAddress2'];
+$inputCity = $_POST['inputCity'];
+//gen fields
+$visDate = date("d")."/".date("m")."/".date("Y");
+$visID = "DRMC"."/".mt_rand(0,10000000);
+
+//Clean up data
+if ($mAProvider == "OTHER"){
+  $mAProvider = $MAOther;
+}
+
+//Compile data entry
+$data =$visID.",".$visDate.",".$pateLastName.",".$pateFirstName.",".$pateIntial.",".$gender.",".$dob
+      .",".$mAProvider.",".$memNumber.",".$suffixNumber.",".$memLastName.",".$memFirstName
+      .",".$r2Member.",".$refDoctor.",".$reqExam.",".$mobNum.",".$email.",".$inputAddress.",".$inputCity;
+
+$file="file.csv";
+
+file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+
+?>
+<html>
+<head>
+  <title>DRMC Patient Log</title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <!--Libraries-->
+  <link rel="stylesheet" href="_lib/bootstrap-4/css/bootstrap.css">
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <header class="col-md-12">
+        <h1>DRMC Patient Registation</h1>
+        <button type="button" class="btn btn-success">Access Data</button>
+      </header>
+    </div>
+    <div class="row">
+      <section class="col-md-12">
+        <p>Patient: <?php echo $pateFirstName?> <?php echo $pateLastName?> has been added to our records with Visit ID: <?php echo $visID?></p>
+      </section>
+      <footer class="col-md-12">
+        <p>Designed by Dumezweni Ndlovu</p>
+      </footer>
+    </div>
+  </div>
+</body>
+</html>
---
+++ _program_source/javaScript/form_post.js
@@ -0,0 +1,38 @@
+function validateForm() {
+    var pLN = document.forms["patientForm"]["pateLastName"].value;
+    var pFN = document.forms["patientForm"]["pateFirstName"].value;
+    var gnder = document.forms["patientForm"]['gender'].value;
+    var dob = document.forms["patientForm"]['dob'].value;
+    var mAidProvider = document.forms["patientForm"]['mAProvider'].value;
+    var mAidNumber = document.forms["patientForm"]['memNumber'].value;
+    var suffixNumber = document.forms["patientForm"]['suffixNumber'].value;
+    var memLastName = document.forms["patientForm"]['memLastName'].value;
+    var memFirstName = document.forms["patientForm"]['memFirstName'].value;
+    var r2Member = document.forms["patientForm"]['r2Member'].value;
+    var refDoctor = document.forms["patientForm"]['refDoctor'].value;
+    var reqExam = document.forms["patientForm"]['reqExam'].value;
+
+    var correctionMSG ="";
+
+    if (pLN == "")  correctionMSG += "Patient's Last Name must be filled out.\n";
+    if (pFN == "")  correctionMSG += "Patient's First Name must be filled out.\n";
+    if (gnder == "") correctionMSG += "A gender must be selected out.\n";
+    if (dob == "") correctionMSG += "Date of Birth must be entered.\n";
+    if (mAidProvider == "--select--") correctionMSG += "Please select a Medical Aid Provider.\n";
+    if (mAidNumber == "") correctionMSG += "Patient's Medical Aid Number must be entered.\n";
+    if (suffixNumber == "") correctionMSG += "Patient's Suffix Number must be recorded.\n";
+    if (memLastName == "") correctionMSG += "Medical Member's Last Name must be filled out.\n";
+    if (memFirstName == "") correctionMSG += "Medical Member's First Name must be filled out.\n";
+    if (r2Member == "--select--") correctionMSG += "Please select the Patient's Relation to the Medical Aid Member.\n";
+    if (refDoctor == "") correctionMSG += "The Referring Doctor must be entered.\n";
+    if (reqExam == "--select--") correctionMSG += "Please select the Requested Procedure.\n";
+
+
+    if (correctionMSG == ""){
+      return true;
+    }
+    else{
+      alert(correctionMSG);
+      return false;
+    }
+}
